9540e656298ca39c59a5f92a6ac487c63e2db95c
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2023-10-08 15:33:27 +0200

    Solved week 6 exercises (#12)
    * Solve exercise 1-4
    
    * Solved week6
    
    * Make argument names readable

--- 060-par/Exercises.scala
+++ 060-par/Exercises.scala
@@ -57,10 +57,9 @@ extension [A] (pa: Par[A])
   
   // Exercise 8
 
-  def chooser[B](f: A => Par[B]): Par[B] = ???
-
+  def chooser[B](f: A => Par[B]): Par[B] = 
+    es => k => Par.eval (es) (f(pa.run(es)) (es) (k))
   // Exercise 9 continues in the Par object, in the bottom of the file
-
 
 
 
@@ -96,24 +95,30 @@ object Par:
   // Exercise 1
   
   /* Write the answer here in a comment 
-   * ...
+   * The reason is that it will wait until the Par type is actually being 
+   * executed. So it uses by name so that it can wait computing the potentially
+   * heavy computation.
    */
   
   // Exercise 2 
   
   def asyncF[A, B](f: A => B): A => Par[B] = 
-    ???
+    f andThen lazyUnit
   
   // Exercise 3
-  
   /* Write the answer here in a comment:
-   * ...
+   * I would first test that the map didn't execute anything, that makes it waiting. We can do this
+   * by creating a function that takes a long time, and then time how long it takes map to return,
+   * the new Par[B]. It should just build our Par structure, so it should be instantly. 
+   * Secondly I would ensure that the mapping function works. We can do that by providing a 
+   * unit(Int) and a mapping function would turn the Int into a string. We would then call run and see
+   * if it returns the valu we expected.
    */
   
   // Exercise 4
 
   def sequence[A](ps: List[Par[A]]): Par[List[A]] =
-    ???
+    ps.foldRight[Par[List[A]]](unit(Nil))((a,b) => a.map2(b)((x,y) => x::y))
 
   /* This is shown in the book: */
   def parMap[A, B](as: List[A]) (f: A => B): Par[List[B]] =
@@ -122,16 +127,29 @@ object Par:
   // Exercise 5
 
   def wget(uris: String*): List[String] =
-    ???
+    val es = java.util.concurrent.Executors.newFixedThreadPool(4)
+    val res = parMap(uris.toList)(uri => scala.io.Source.fromURL(uri)("ISO-8859-1").mkString).run(es)
+    es.shutdown()
+    res
 
   /* Write your explanation in English here:
-   * ...
+   * This runs in parallel because we use the previous parMap, that given a
+   * list of items, maps items in parallel, by using the asyncf 
    */
 
   // Exercise 6
 
   def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] =
-    ???
+
+    def g(a:A): Option[A] =
+      if f(a) then
+        Some(a)
+      else None
+
+    as.map(asyncF(g)).foldRight[Par[List[A]]](unit(Nil))((x,y) => x.map2(y)((a,b) => a match
+      case None => b
+      case Some(value) => value::b
+    ))
 
   // Exercise 7
 
@@ -139,10 +157,13 @@ object Par:
   // It has to be adapted to the non-blocking representation of Par and
   // Future that we are using in this chapter.
   def choiceN[A](pn: Par[Int])(choices: List[Par[A]]): Par[A] =
-    ???
+    pn.map2(sequence(choices))((idx, ls) => ls(idx))
  
   def choice[A](pb: Par[Boolean])(t: Par[A], f: Par[A]): Par[A] =
-    ???
+    def btoi(pb: Par[Boolean]): Par[Int] =
+      pb.map(x => if x then 0 else 1)
+
+    choiceN(btoi(pb))(t::f::Nil)
 
   // Exercise 8 is found above, in the extension methods of Par[A]
   // Come back here when done with Exercise 8.
@@ -150,10 +171,10 @@ object Par:
   // Exercise 9
  
   def join[A](p: Par[Par[A]]): Par[A] =
-    ???
+    p.chooser(x => x)
  
   def chooser[A, B](p: Par[A])(f: A => Par[B]): Par[B] =
-    ???
+    join(p.map(x => f(x)))
 
 end Par
 
@@ -164,20 +185,21 @@ end Par
 
 // The types here are broken, and you need to fix them
 // replace THIS with a meaningful name
-extension (THIS: Any) 
-  def join: Any = 
-    ???
+extension [A] (papa: Par[Par[A]]) 
+  def join: Par[A] = 
+    Par.join(papa)
 
 // The types here are broken, and you need to fix them
-extension (THIS: Any) 
-  def choiceN (SOMETHING: Any): Any = 
-    ???
+extension (pa: Par[Int]) 
+  def choiceN[A] (pb: List[Par[A]]): Par[A] = 
+    Par.choiceN(pa)(pb)
 
 // The types here are broken, and you need to fix them
-extension (THIS: Any) 
-  def choice (SOMETHING: Any, ANOTHERTHING: Any) = 
-    ???
+extension (pa: Par[Boolean]) 
+  def choice[A] (f: Par[A], g: Par[A]) = 
+      Par.choice(pa)(f, g)
 
 /* Write your answer in English here:
- * ....
+ * This is due to the different type parameter which 
+ * we cannot guarantee a common supertype for
  */

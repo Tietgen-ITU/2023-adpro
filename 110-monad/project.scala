07af3987ff91e765a82b9db9f73da70fa8c2148f
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2023-12-24 15:11:29 +0100

    Change the compiled version of the project

--- 110-monad/project.scala
+++ 110-monad/project.scala
@@ -1,4 +1,4 @@
-//> using scala 3.3.0
+//> using scala 3.3.1
 //> using options -Xfatal-warnings, -deprecation, -feature, -source:future, -language:adhocExtensions
 //> using dep "org.scalacheck::scalacheck:1.17.0"
 //> using lib "org.scalactic::scalactic:3.2.17"

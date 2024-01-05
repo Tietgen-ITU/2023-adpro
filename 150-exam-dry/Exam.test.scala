482b77d5b621a30e0d9ce33b2a7609045e17df79
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2024-01-05 10:57:17 +0100

    Add test for question 1

--- 150-exam-dry/Exam.test.scala
+++ 150-exam-dry/Exam.test.scala
@@ -8,11 +8,19 @@ import org.scalacheck.{Gen, Arbitrary}
 import org.scalacheck.Arbitrary.arbitrary
 import org.scalacheck.Prop.{forAll, forAllNoShrink}
 
+import adpro.laziness.*
+
 object ExamSpec
   extends org.scalacheck.Properties("exam-2023-autumn"):
 
+  def toAdproLazyList[A](xs: List[A]): LazyList[A] =
+    xs.foldRight(LazyList.empty)((x, acc) => LazyList.cons(x, acc))
+
   property("A test that always passes (a sanity check)") = 
     forAll { (n: Int) => n == n }
+
+  property("Q1: Test that they give the same answer") =
+    forAll((xs: List[Int]) => Streaming.fViaRec(toAdproLazyList(xs)) == Streaming.fViaFold(toAdproLazyList(xs)))
 
 end ExamSpec
 
